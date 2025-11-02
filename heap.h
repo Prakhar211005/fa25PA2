493f2b93ea1861599892b53493eac4af7aa218f4
Prakhar <psahu9215@sdsu.edu>
2025-11-01 22:52:22 -0700

    Set up the cloned repo. Implemented MinHeap push,pop, upheap and downheap

--- heap.h
+++ heap.h
@@ -15,21 +15,49 @@ struct MinHeap {
     MinHeap() { size = 0; }
 
     void push(int idx, int weightArr[]) {
-        // TODO: insert index at end of heap, restore order using upheap()
+        if (size >=64) return;
+        data[size] = idx;
+        upheap(size, weightArr);
+        size++;
     }
 
     int pop(int weightArr[]) {
-        // TODO: remove and return smallest index
-        // Replace root with last element, then call downheap()
-        return -1; // placeholder
+        if (size == 0) return -1;
+        int root = data[0];
+        size--;
+        data[0] = data[size];
+        downheap(0, weightArr);
+        return root;
     }
 
     void upheap(int pos, int weightArr[]) {
-        // TODO: swap child upward while smaller than parent
+        while (pos > 0) {
+            int parent = (pos - 1) / 2;
+            if (weightArr[data[pos]] < weightArr[data[parent]]) {
+                swap(data[pos], data[parent]);
+                pos = parent;
+            } else {
+                break;
+            }
+        }
     }
 
     void downheap(int pos, int weightArr[]) {
-        // TODO: swap parent downward while larger than any child
+        while (true) {
+            int left  = 2 * pos + 1;
+            int right = 2 * pos + 2;
+            int smallest = pos;
+
+            if (left < size && weightArr[data[left]] < weightArr[data[smallest]])
+                smallest = left;
+            if (right < size && weightArr[data[right]] < weightArr[data[smallest]])
+                smallest = right;
+
+            if (smallest == pos) break;
+
+            swap(data[pos], data[smallest]);
+            pos = smallest;
+        }
     }
 };
 
